cd0a53e20bfbc14d6a5face81b2de33bdb157fc4
Muhammet <atayevakmuhammet77@gmail.com>
2023-02-12 00:37:00 +0300

    ''

--- 6-занятие/аэродинамическое сопротивление.py
+++ 6-занятие/аэродинамическое сопротивление.py
@@ -1,54 +1,18 @@
-"""На расход топлива в конкретном автомобиле влияет множество факторов.
-Наиболее весомые – это характеристики двигателя,
-трансмиссии, тип КП, конструктивные особенности и др.)
-и внешние факторы сопротивления движению.
-    К последним относится и аэродинамическое сопротивление (F_АС).
-    Этот показатель вычисляется по формуле,которая учитывает и плотность воздуха,
-и коэффициент лобового сопротивления (Сх), конкретного авто,
-и лобовую площадь (S) автомобиля (площадь поперечного сечения),
-и скорость движения (V). Причем именно она является наиболее существенной,
-поскольку ее значение в формуле – «в квадрате».
-    При выборе крупного автомобиля, да еще и с посредственными показателями
-обтекаемости, следует помнить, что экономить горючее на нем можно только в ущерб
-скорости движения.
-
+"""Подсчитать аэродинамическое сопротивление выбранной вами марки автомобиля
+на скоростях от 25 км/ч до 250 км,ч с шагом 10 км/ч. Создайте класс, инициализируйте конструктор,
+определите функциональный метод и экземпляр с выводом.
+Из полученных данных составьте график кривой соотношения F_AC/V.
 # Формула:
 F_АС= Cx*S*(V**2*ρ/2)
-# Параметрические данные:
 V - скорость (м/с)
 ρ - плотность воздуха в тропосфере у земли = 1.23 кг,м**3
-Cx (коэффициент АС) = F/(Q*S) (для известных марок авто выберите показатели (Сх) с сайта:
-https://topruscar.ru/terminy/koeffitsient-obtekaemosti)
-
-Q (скоростной напор) = (1.23*V**2)/2
-
-Условие: подсчитать аэродинамическое сопротивление выбранной вами марки автомобиля
-на скоростях от 25 км/ч до 250 км,ч
-с шагом 10 км/ч. Создайте класс, инициализируйте конструктор, определите функциональный метод и экземпляр с выводом.
-Из полученных данных составьте график кривой соотношения F_AC/V.
-Определите мах F_AC c оответствующим скоростным показателем.
-Альтернативно можно составить для любых, введенных пользователем скоростей.
-
-import sympy as sp
-import numpy as np
-import math as mt
-import matplotlib.pyplot as plt
-
-C = sp.Symbol('Cx')
-S, V, p = sp.symbols("S, V, ρ")
-p= 1.23
-S=в сети найдмте для вашего авто
-V=[]
-C= на выбор https://topruscar.ru/terminy/koeffitsient-obtekaemosti)
-F_АС= (C*S)*(V**2*p/2)
-print(F_АС)
+Cx (коэффициент АС)
 """
 # Atayev Akmuhammet
 # Lab 6
 
-
 import matplotlib.pyplot as plt
-import math as m
+import numpy as np
 
 class Aero_drag:
 
@@ -58,27 +22,39 @@ class Aero_drag:
         self.s = s
         self.p = p
 
+    # метод итерация для скорости: (25-250 по 10) км/ч => м/с
     def __iter__(self):
-        return range(25, 255, 10)
+        return list(map(float, [float(i) for i in np.arange(6.94, 70.83, 2.77)]))
 
-    def calculate(self):
-        for i in self.__iter__():
-            return list(map((lambda i: (self.cx * self.s * (i**2) * self.p) / 2),
-                            [float(i) for i in self.__iter__()]))
+    # МЕТОД РАСЧЕТА F_AC В СКОРОСТЯХ: (25-250 по 10) км/ч
+    def F_AC(self):
+        return list(map(lambda i: (self.cx * self.s * (i ** 2) * self.p) / 2,
+                        [float(i) for i in self.__iter__()]))
 
+    # результат
+    def get_F_AC(self):
+        return f'Aero drag of {self.model} = {self.F_AC()}'
 
-    def __getitem__(self):
-        return f'Aero drag of {self.model} = {self.calculate()}'
+    # МЕТОД РАСЧЕТА F_AC/V: (25-250 по 10) км/ч
+    def divide_to_speed(self):
+        return list(map(lambda x, y: x / y, self.F_AC(), self.__iter__()))
+
+    # результат F_AC/V
+    def get_divide_to_speed(self):
+        return f'F_AC / V = {self.divide_to_speed()}'
 
 # --------------------
-car = Aero_drag('BMW 518i E28', 0.39, 0.30)
-print(car.__getitem__())
+car = Aero_drag('BMW 518i E28', 0.39, 0.30)     #atributes = model, cx, s
+
+# --------- ГРАФИК F_AC В СКОРОСТЯХ: (25-250 по 10) км/ч
+plt.title('F_AC в скоростях: (25-250 по 10) км/ч')
+plt.ylabel('V м/с')
+plt.stem((car.__iter__()), (car.F_AC()))
+plt.show()
 
 
-graph = map(lambda v: [float(i)/v for i in car.calculate()], [float(v) for v in car.__iter__()])
-graph = list(graph)
-print(graph)
-
-plt.plot((graph), car.__iter__())
-# plt.scatter((graph), car.__iter__())
+# ---------- ГРАФИК F_AC/V В СКОРОСТЯХ: (25-250 по 10) км/ч
+plt.title('F_AC / V в скоростях: (25-250 по 10) км/ч')
+plt.ylabel('V м/с')
+plt.stem((car.__iter__()), (car.divide_to_speed()))
 plt.show()
