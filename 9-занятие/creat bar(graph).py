7f5afd790eaad8eb817739f447e317fab3b57c04
Muhammet <atayevakmuhammet77@gmail.com>
2023-03-06 18:30:34 +0300

    'new'

--- 9-занятие/creat bar(graph).py
+++ 9-занятие/creat bar(graph).py
@@ -8,6 +8,7 @@
 
 import matplotlib.pyplot as plt
 
+
 class Graph:
     def __init__(self, x, y):
         self.x = x
@@ -17,9 +18,16 @@ class Graph:
         return plt.bar(self.x, self.y), plt.show()
 
 
-b = Graph([float(x) for x in input('X: (0 - 100) = ').split()],
-          [float(y) for y in input('Y: (-100 - 100) = ').split()]).graph_plot()    # input
+try:
+    # input
+    b = Graph([float(x) if 0 < int(x) < 100 else ValueError for x in input('X: (0 - 100) = ').split()],
+              [float(y) if -100 < int(y) < 100 else ValueError for y in
+               input('Y: (-100 - 100) = ').split()]).graph_plot()
+
+    # random list
+    # b = Graph(np.random.uniform(low=0, high=100, size=50),
+    #           np.random.uniform(low=-100, high=100, size=50)).graph_plot()
 
 
-# b = Graph(np.random.uniform(low=0, high=100, size=50),
-#           np.random.uniform(low=-100, high=100, size=50)).graph_plot()              #random list
+except:
+    print('Value error')
